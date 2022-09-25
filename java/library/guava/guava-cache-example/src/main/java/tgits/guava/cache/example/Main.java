ea40e4e24509c124fb7dc152a3d90e7875695fce
TGITS <thegeekintheshell@gmail.com>
2022-09-25 17:44:13 +0200

    feat(init_guava)
    Initialisation de devtoolbox avec 2 projets pour contenir des exemples de code avec Guava (cache et memoisation)

---
+++ java/library/guava/guava-cache-example/src/main/java/tgits/guava/cache/example/Main.java
@@ -0,0 +1,8 @@
+package tgits.guava.cache.example;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Cache with Guava Examples");
+    }
+}
---
+++ java/library/guava/guava-memoization-example/src/main/java/tgits/guava/memoization/example/Main.java
@@ -0,0 +1,8 @@
+package tgits.guava.memoization.example;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Memoization with Guava Examples");
+    }
+}
