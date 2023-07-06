660986c2cc8a63d4a4aacb95d19caea93e06db45
TGITS <thegeekintheshell@gmail.com>
2023-07-06 14:06:57 +0200

    feat(init_new_projects)
    Initialisation de 2 nouveaux projets de code
    MAJ de la version de Datafaker dans datafaker-example

---
+++ java/tool/dus/src/test/java/tgits/devtoolbox/dus/DusApplicationTests.java
@@ -0,0 +1,13 @@
+package tgits.devtoolbox.dus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
