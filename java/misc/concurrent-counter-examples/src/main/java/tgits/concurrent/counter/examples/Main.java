ebd76db72e44fbf92b92958981117367b91aa195
TGITS <thegeekintheshell@gmail.com>
2024-07-17 19:17:13 +0200

    feat(concurrent_counter) Petite correction de code Nettoyage en vue de la préparation de l'article

--- java/misc/concurrent-counter-examples/src/main/java/tgits/concurrent/counter/examples/Main.java
+++ java/misc/concurrent-counter-examples/src/main/java/tgits/concurrent/counter/examples/Main.java
@@ -3,43 +3,26 @@ package tgits.concurrent.counter.examples;
 import org.apache.commons.lang3.time.StopWatch;
 import tgits.concurrent.counter.examples.counter.*;
 import tgits.concurrent.counter.examples.worker.AdderWorker;
-import tgits.concurrent.counter.examples.worker.LetterCounterWorker;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.LongAdder;
 
 public class Main {
 
     public static void main(String... args) throws InterruptedException {
         System.out.println("Code examples with updated counter in concurrent environment");
         LongCounter notSynchronizedLongCounter = new NotSynchronizedLongCounter("notSynchronizedLongCounter");
-        runFirstExperiment(notSynchronizedLongCounter);
+        updateCounterFromThreads(notSynchronizedLongCounter);
 
         LongCounter synchronizedLongCounter = new SynchronizedLongCounter("synchronizedLongCounter");
-        runFirstExperiment(synchronizedLongCounter);
-
-        LongCounter volatileLongCounter = new VolatileLongCounter("volatileLongCounter");
-        runFirstExperiment(volatileLongCounter);
+        updateCounterFromThreads(synchronizedLongCounter);
 
         LongCounter longAdderLongCounter = new LongAdderLongCounter("longAdderLongCounter");
-        runFirstExperiment(longAdderLongCounter);
+        updateCounterFromThreads(longAdderLongCounter);
 
         LongCounter atomicLongLongCounter = new AtomicLongLongCounter("atomicLongLongCounter");
-        runFirstExperiment(atomicLongLongCounter);
+        updateCounterFromThreads(atomicLongLongCounter);
 
-        final List<String> strings = List.of("A", "B", "C", "A", "B", "A", "D", "E", "F");
-
-        Map<String, LongAdder> frequencies = new HashMap<>();
-        runSecondExperiment(frequencies, strings);
-
-        Map<String, LongAdder> concurrentFrequencies = new ConcurrentHashMap<>();
-        runSecondExperiment(concurrentFrequencies, strings);
     }
 
-    public static void runFirstExperiment(LongCounter longCounter) throws InterruptedException {
+    public static void updateCounterFromThreads(LongCounter longCounter) throws InterruptedException {
         System.out.println(longCounter.name() + " initial value : " + longCounter.value());
         Thread t1 = new Thread(new AdderWorker(longCounter));
         Thread t2 = new Thread(new AdderWorker(longCounter));
@@ -56,26 +39,5 @@ public class Main {
         System.out.println(longCounter.name() + " value after counting with workers ends : " + longCounter.value());
         System.out.println("Time Elapsed: " + watch.getTime());
         System.out.println("\n---------------------\n");
-    }
-
-    public static void runSecondExperiment(Map<String, LongAdder> frequencies, List<String> strings) throws InterruptedException {
-        Thread t1 = new Thread(new LetterCounterWorker(frequencies, strings));
-        Thread t2 = new Thread(new LetterCounterWorker(frequencies, strings));
-        Thread t3 = new Thread(new LetterCounterWorker(frequencies, strings));
-        StopWatch watch = new StopWatch();
-        watch.start();
-        t1.start();
-        t2.start();
-        t3.start();
-        t1.join();
-        t2.join();
-        t3.join();
-        watch.stop();
-        for (String key : frequencies.keySet()) {
-            System.out.println(key + ": " + frequencies.get(key));
-        }
-        System.out.println("Time Elapsed: " + watch.getTime());
-        System.out.println("\n---------------------\n");
-
     }
 }
