143a81f0d7e53d1b7eb88dd2096c5417ff833925
TGITS <thegeekintheshell@gmail.com>
2024-11-03 16:43:55 +0100

    Petites corrections formelles

--- java/misc/concurrent-frequencies-map-examples/src/main/script/heroes-frequencies-map-multi.java
+++ java/misc/concurrent-frequencies-map-examples/src/main/script/heroes-frequencies-map-multi.java
@@ -17,8 +17,8 @@ class FrequenciesMapUpdater implements Runnable {
     private final List<String> lines;
     private final Map<String, LongAdder> frequencies;
 
-    public FrequenciesMapUpdater(final List<String> elements, final Map<String, LongAdder> frequencies) {
-        this.lines = elements;
+    public FrequenciesMapUpdater(final List<String> lines, final Map<String, LongAdder> frequencies) {
+        this.lines = lines;
         this.frequencies = frequencies;
     }
 
@@ -29,12 +29,13 @@ class FrequenciesMapUpdater implements Runnable {
         lines.remove(0);
         for (String line : lines) {
             //System.out.println(line);
-            String[] elements = line.split(",");
-            //For the code example, we only need the publisher, which is the column of index 6 (the 7th)
-            frequencies.computeIfAbsent(elements[6].strip(), k -> new LongAdder()).increment();
+            String[] items = line.split(",");
+            //For the code example, we only need the publisher, which is the column of index 6 (the 7th column)
+            frequencies.computeIfAbsent(items[6].strip(), k -> new LongAdder()).increment();
             try {
                 Thread.sleep(Duration.ofMillis(10));
             } catch (InterruptedException ie) {
+                //A minimalist management of the Exception not suitable in a real project
                 ie.printStackTrace();
             }
         }
@@ -44,27 +45,31 @@ class FrequenciesMapUpdater implements Runnable {
 class Main {
 
     public static void main(String... args) throws IOException, InterruptedException {
-        System.out.println("Code example with updated frequencies map in a concurrent environment");
+        System.out.println("Code example with a frequency map in a concurrent environment");
         final Map<String, LongAdder> frequencies = new ConcurrentHashMap<>();
-        System.out.println("The frequencies map is initially empty : frequencies.isEmpty() : " + frequencies.isEmpty());
+        System.out.println("The frequencies map is initially empty - frequencies.isEmpty() : " + frequencies.isEmpty());
 
         List<Thread> workers;
 
+        //The list of threads is created from the stream of pathes.
+        //The lines of each file in the provided directory are extracted and given to one of the thread
         try (Stream<Path> pathes = Files.list(Path.of("./data/multi"))) {
             workers = pathes
                     .map(path -> createThread(path, frequencies))
                     .collect(Collectors.toList());
         }
 
+        //Each thread is started
         for (Thread worker : workers) {
             worker.start();
         }
 
+        //Waiting for the end of the work done by the threads
         for (Thread worker : workers) {
-            //Waiting for the end of the work done by the threads
             worker.join();
         }
 
+        //Displaying the content of the frequency map
         for (Map.Entry<String, LongAdder> entry : frequencies.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
@@ -72,6 +77,7 @@ class Main {
         System.out.println("\n---------------------\n");
     }
 
+    //Utility method to manage the IOException
     private static Thread createThread(final Path path, final Map<String, LongAdder> frequencies) {
         try {
             return new Thread(new FrequenciesMapUpdater(Files.readAllLines(path), frequencies));
@@ -79,5 +85,4 @@ class Main {
             throw new RuntimeException(ex);
         }
     }
-
 }
